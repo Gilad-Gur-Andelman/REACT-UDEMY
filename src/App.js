b4cb265449eec09c5a52da71d0265b3e281e79b2
Gilad Gur Andelman <giladgurandelman@Gilads-MacBook-Pro.local>
2019-12-02 18:17:13 +0200

    outputting dynamic content

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi Arnon Peleg</h1>
+        <h1>Hi</h1>
         <p>Is this working?</p>
-        <Person />
+        <Person name="Gilad" age="33" />
+        <Person name="Arnon" age="45" >(what a loser)</Person>
+        <Person name="Peleg" age="76" />
       </div>
     );
   }
