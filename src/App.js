c85fa2e81a828f3951f6ccec3863cd784cf4907e
Gilad Gur Andelman <giladgurandelman@Gilads-MacBook-Pro.local>
2019-12-03 12:17:52 +0200

    add button to change state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Gilad', age: 33 },
+      { name: 'Arnon', age: 45 },
+      { name: 'Peleg', age: 76 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Gilad Gur', age: 33 },
+        { name: 'Arnon', age: 45 },
+        { name: 'Peleg', age: 109 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi</h1>
         <p>Is this working?</p>
-        <Person name="Gilad" age="33" />
-        <Person name="Arnon" age="45" >(what a loser)</Person>
-        <Person name="Peleg" age="76" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
